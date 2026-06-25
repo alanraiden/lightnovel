8796c31260ce2d7e3f616775b6c48405a1f2ce9e
alan <142570968+alanraiden@users.noreply.github.com>
2026-06-26 03:45:05 +0530

    Delete src directory

--- src/app/about/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "About Us",
-};
-
-export default function AboutLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
